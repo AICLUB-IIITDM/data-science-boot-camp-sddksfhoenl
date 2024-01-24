a4bc7c0a2a6d1ba75cccd4cac5988dbc826574a1
sddksfhoenl <157616895+sddksfhoenl@users.noreply.github.com>
2024-01-24 15:23:24 +0000

    solved

--- day01/Math.py
+++ day01/Math.py
@@ -1,3 +1,12 @@
 # write a function to implement addition
 def add(a,b):
+    print("the two numbers are",a,b)
+    c=a+b
+    print("the sum of the two is",c)
+    return c
     pass
+
+a = 5
+b= 8
+v = add(a,b)
+print(v)
